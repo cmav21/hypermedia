8f856a7475d9829c5e619eb301710f707585a451
cesar magallon <cesar9mv@gmail.com>
2019-09-02 20:48:48 -0500

    [add]packages

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const app = express();
-
+const passport = require("passport");
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req, res)=>{
-    res.send('<html><h1>bye</h1></html>')
-});
+const app = express();
 
+// passport.use(new GoogleStrategy());
+
+app.get('/',(req, res)=>{
+    res.send('<html><h1>te quiero mucho cesario</h1></html>')
+});
 
 app.listen(PORT, ()=> {
     console.log("Escuchando en el puerto 3000")
-})
+});
