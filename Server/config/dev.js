b4c5a9df7cff1501414de2cca5cedc53d917d9ed
20132867 CESAR AUGUSTO MAGALLON VADILLO <20132867@TELEMATICA.UCOL.MX>
2019-09-25 07:59:53 -0500

    sdkf

---
+++ Server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '938754991923-3sdkubnkmhc76uafk3scbb54844pbqtm.apps.googleusercontent.com',
+    googleClientSecret: 'Iei-3URMffKAjMP9hNAu3NhG',
+    mongoURI: 'mongodb://diana:diana1@ds149763.mlab.com:49763/emaily-dev-diana',
+    cookiekey: 'fkdsjfklsjfksjfskjfjfsdjkfsjkf'
+};
