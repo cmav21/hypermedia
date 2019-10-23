80691cb70cfe3537dedc84e949a84477f9e320b3
20134315 AURELIA ALEJANDRA PEREZ GOMEZ <20134315@TELEMATICA.UCOL.MX>
2019-10-23 08:05:54 -0500

    refactor promesas

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,3 +1,8 @@
+// identificar funcion con accion asincrona
+// enfrende de esa funcion  poer la palabra reservada async
+// identificar segmento de codigo con promesa y poner la palabra reservada await
+// asignar await a una variable
+
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
@@ -23,16 +28,13 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-            User.findOne({googleId: profile.id}).then(existingUser => {
-				if(existingUser) {
-					done(null, existingUser);
-				} else {
-					new User({ googleId: profile.id }).save().then(user => {
-						done(null, user);
-					})
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({googleId: profile.id})
+			if(existingUser) {
+				return done(null, existingUser);
+			} 
+			user = await new User({ googleId: profile.id }).save();
+			done(null, user);
         }
 	)
 );
