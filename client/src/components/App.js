461b18be1ab26f8e2a573997c384e0b6310f1fb7
20134315 AURELIA ALEJANDRA PEREZ GOMEZ <20134315@TELEMATICA.UCOL.MX>
2019-10-30 08:27:10 -0600

    add materialize

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+
+const Dashboard = () => <h2>Dashboard</h2>
+const Landing = () => <h2>Landing</h2>
+const SurveryNew = () => <h2>SurveryNew</h2>
 
 const App = () => (
     <div>
-        Hi there!
+        <BrowserRouter>
+            <Header />
+            <Route exact path="/" component={Landing}/>
+            <Route exact path="/surveys" component={Dashboard}/>
+            <Route exact path="/surveys/new" component={SurveryNew}/>
+        </BrowserRouter>
     </div>
 );
 
